29d88c33e8681a3115dde84f636a82bcd3f0c704
Marcys <moovspace@github.com>
2020-01-24 20:46:56 +0100

    Vue Composite Api (@onclick, props, template)

---
+++ src/components/CompositeApi.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="hello">
+        <h1> {{ user }} </h1>
+        <button @click="appendName"> Click me </button>
+    </div>
+</template>
+
+<script>
+import { ref } from '@vue/composition-api';
+import { computed } from '@vue/composition-api';
+
+export default {
+    props: {
+        name: String
+    },
+    setup(props) {
+        // console.log(props.name);
+        // context.attrs
+        // context.slots
+        // context.emit
+        // context.parent
+        // context.root
+
+        const user = ref(props.name);
+
+        const appendName = () => {
+            user.value = `Hello ${props.name}`
+        }
+
+        const plusOne = computed(() => {
+            user.value = `Hello Miszczu ${props.name}`
+        })
+
+        return { user, appendName, plusOne }
+    }
+}
+</script>
+
+<style scoped>
+.helo{
+    font-weight: 22px; color: #09f;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <CompApi name="Adam Wielki" />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import CompApi from '@/components/CompositeApi.vue'
+
+export default {
+  name: 'about',
+  components: {
+    CompApi
+  }
+}
+</script>
